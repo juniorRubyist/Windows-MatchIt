892f9867182035cd8e1d9683e11664bcec86d761
jgeis <geis28@gmail.com>
2013-11-28 10:41:46 -0800

    Adding a list of "Webdings" Font characters to make the computer choose from. (No Click Handler Yet!)

---
+++ MatchingGame/MatchingGame/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Webdings font,
+        // and each icon appears twice in this lis
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
